1147011001f895019c0d3b781f864974d95207a6
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2017-08-04 11:29:44 +0200

    Add C++ header-only bindings for 0MQ

---
+++ cppzmq.rb
@@ -0,0 +1,29 @@
+# basically a copy of jmuncaster's tap @
+# https://github.com/jmuncaster/homebrew-header-only/blob/master/cppzmq.rb
+class Cppzmq < Formula
+  desc "C++ bindings for ZeroMQ"
+  homepage "https://github.com/zeromq/cppzmq"
+  url "https://codeload.github.com/zeromq/cppzmq/tar.gz/v4.2.2"
+  sha256 "9a26f083f0d0678a3cdbcacb9ca0a193cedca44551a089cce724c0c8538ded59"
+  depends_on "zeromq"
+
+  def install
+    include.install "zmq.hpp"
+    include.install "zmq_addon.hpp"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <zmq.hpp>
+      #include <iostream>
+      int main()
+      {
+        int major=0, minor=0, patch=0;
+        zmq::version(&major, &minor, &patch);
+        std::cout << major << "." << minor << "." << patch << std::endl;
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-I#{include}", "-L#{lib}", "-lzmq", "-o", "test"
+  end
+end
